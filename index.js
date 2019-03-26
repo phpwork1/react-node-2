4577494e2cc64e904e2d80ec83b0b6f893e8e6b9
Saputra <Joshua.Saputra@aia.com>
2019-03-26 16:00:20 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/key');
+require('./models/User');
+require('./models/Survey');
+require('./services/passport');
+const app = express();
+
+//body parser make payload available in req.body within express post request
+app.use(bodyParser.json());
+//activate cookie session
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//connect mongoose into MLab (MongoDB URI)
+mongoose.connect(keys.mongoURI);
+
+//use routes with app
+require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
+
+if(process.env.NODE_ENV === 'production'){
+    //Express will serve production assets
+    //main.js or main.css files
+    app.use(express.static('client/build')); //search dir client/build for static files
+
+    //Express will serve index.html
+    //if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
---
+++ services/passport.js
@@ -0,0 +1,42 @@
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const key = require('../config/key');
+
+//get mongoose model declared in models/User.js
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    //Find user with id
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        });
+});
+
+passport.use(
+    new googleStrategy({
+            clientID: key.googleClientID,
+            clientSecret: key.googleClientSecret,
+            callbackURL: '/auth/google/callback',
+            proxy: true,
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            //find user with googleId of profile.id
+            const existingUser = await User.findOne({googleId: profile.id});
+            if (existingUser) {
+                //We already have a record with the given profile Id
+                return done(null, existingUser); //
+            }
+            //we don't have a record with the given profile Id
+            //create user who has a googleId of profile.id
+            const user = await new User({googleId: profile.id}).save();
+            done(null, user);
+
+        }
+    )
+);
